e69132b6919426f1d0b53183f1600231818de9da
Arthur Kay <arthurakay@gmail.com>
2016-08-05 14:12:53 -0500

    removing unneeded constructor

--- ui/jsx/Message.js
+++ ui/jsx/Message.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
 class Message extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div className="inboxMsg">
